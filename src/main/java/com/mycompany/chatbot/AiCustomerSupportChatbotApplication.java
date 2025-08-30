1e1603ca527abe5dab7276016961134c0c69ceef
Ksenia <kseniasjava@gmail.com>
2025-08-30 14:41:46 +0300

    First commit

---
+++ src/main/java/com/mycompany/chatbot/AiCustomerSupportChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCustomerSupportChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCustomerSupportChatbotApplication.class, args);
+    }
+
+}
