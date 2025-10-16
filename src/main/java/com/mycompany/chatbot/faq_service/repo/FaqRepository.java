74282081b5fd56105c57a3247d1d320082e49d2a
Ksenia <kseniasjava@gmail.com>
2025-10-16 11:03:04 +0300

    feat: implement basic faq service methods and create repository

--- src/main/java/com/mycompany/chatbot/faq_service/repo/FaqRepository.java
+++ src/main/java/com/mycompany/chatbot/faq_service/repo/FaqRepository.java
@@ -1,4 +1,10 @@
 package com.mycompany.chatbot.faq_service.repo;
 
-public class FaqRepository {
+import com.mycompany.chatbot.faq_service.domain.Faq;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FaqRepository extends JpaRepository<Faq, Long> {
+    List<Faq> findByQuestionContainingIgnoreCase(String text);
 }
--- src/main/java/com/mycompany/chatbot/faq_service/service/FaqService.java
+++ src/main/java/com/mycompany/chatbot/faq_service/service/FaqService.java
@@ -1,4 +1,40 @@
 package com.mycompany.chatbot.faq_service.service;
 
+import com.mycompany.chatbot.faq_service.domain.Faq;
+import com.mycompany.chatbot.faq_service.repo.FaqRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FaqService {
+    private FaqRepository faqRepository;
+
+    public FaqService(FaqRepository faqRepository) {
+        this.faqRepository = faqRepository;
+    }
+
+    public Faq saveFaq(Faq faq) {
+        return faqRepository.save(faq);
+    }
+
+    public Faq findFaqById(Long id) {
+        return faqRepository.findById(id).orElse(null);
+    }
+
+    public void deleteFaq(Long id) {
+        faqRepository.deleteById(id);
+    }
+
+    public Faq findFaqByQuestion(String question) {
+
+    }
+
+    public Faq getAnswerForQuestion(String question) {
+
+    }
+
+    public Faq updateAnswer(Long id, String newAnswer) {
+        Faq faq = findFaqById(id);
+        faq.setAnswer(newAnswer);
+        return faqRepository.save(faq);
+    }
 }
